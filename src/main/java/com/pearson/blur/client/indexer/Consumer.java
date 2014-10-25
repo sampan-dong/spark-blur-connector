b0f29cf7b252a18fa197295fdc6bc325bce92179
Dibyendu Bhattacharya <dibyendu.bhattacharya1@pearson.com>
2014-10-25 12:30:37 +0530

    Added apache v2 license

--- src/main/java/com/pearson/blur/client/indexer/Consumer.java
+++ src/main/java/com/pearson/blur/client/indexer/Consumer.java
@@ -1,5 +1,22 @@
 package com.pearson.blur.client.indexer;
 
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.Serializable;
@@ -88,9 +105,9 @@ public class Consumer implements Serializable {
 	private void run() {
 
 		String checkpointDirectory = "hdfs://10.252.5.113:9000/user/hadoop/spark";
-		
-		//number of partition for Kafka Topic
-		
+
+		// number of partition for Kafka Topic
+
 		int _partitionCount = 5;
 
 		List<JavaDStream<MessageAndMetadata>> streamsList = new ArrayList<JavaDStream<MessageAndMetadata>>(
@@ -172,6 +189,9 @@ public class Consumer implements Serializable {
 					public Void call(JavaPairRDD<Text, BlurMutate> rdd,
 							Time time) throws Exception {
 
+						/*
+						 * Blur Table Details
+						 */
 						TableDescriptor tableDescriptor = new TableDescriptor();
 						String tableUri = new Path(
 								"hdfs://10.252.5.113:9000/blur/tables/nrt")
@@ -203,8 +223,7 @@ public class Consumer implements Serializable {
 						conf.setClass("mapreduce.reduce.class",
 								DefaultBlurReducer.class, Reducer.class);
 						conf.setClass("mapreduce.outputformat.class",
-								BlurOutputFormat.class,
-								OutputFormat.class);
+								BlurOutputFormat.class, OutputFormat.class);
 						conf.setClass("mapreduce.partitioner.class",
 								BlurPartitioner.class, Partitioner.class);
 						conf.set("mapred.output.committer.class",
@@ -221,11 +240,6 @@ public class Consumer implements Serializable {
 						jobConf.setOutputKeyClass(Text.class);
 						jobConf.setOutputValueClass(BlurMutate.class);
 
-						//int reducerMultiplier = 3;
-
-						//conf.setInt("blur.output.reducer.multiplier",
-						//		reducerMultiplier);
-
 						BlurMapReduceUtil.addAllJarsInBlurLib(conf);
 						BlurMapReduceUtil
 								.addDependencyJars(
@@ -241,8 +255,8 @@ public class Consumer implements Serializable {
 
 						if (pRdd.count() > 0)
 							pRdd.saveAsNewAPIHadoopFile(tableUri, Text.class,
-									BlurMutate.class,
-									BlurOutputFormat.class, jobConf);
+									BlurMutate.class, BlurOutputFormat.class,
+									jobConf);
 
 						return null;
 					}
--- src/main/java/com/pearson/blur/client/indexer/ConsumerEnQueue.java
+++ src/main/java/com/pearson/blur/client/indexer/ConsumerEnQueue.java
@@ -1,5 +1,22 @@
 package com.pearson.blur.client.indexer;
 
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.Serializable;
@@ -81,9 +98,9 @@ public class ConsumerEnQueue implements Serializable {
 	private void run() {
 
 		String checkpointDirectory = "hdfs://10.252.5.113:9000/user/hadoop/spark";
-		
-		//number of partition for Kafka Topic
-		
+
+		// number of partition for Kafka Topic
+
 		int _partitionCount = 5;
 
 		List<JavaDStream<MessageAndMetadata>> streamsList = new ArrayList<JavaDStream<MessageAndMetadata>>(
@@ -146,15 +163,18 @@ public class ConsumerEnQueue implements Serializable {
 
 						String message = new String(mmeta.getPayload());
 						String keyStr = DigestUtils.shaHex(message);
-						
+
 						Record record = new Record();
 						record.setRecordId(keyStr);
 						record.addToColumns(new Column("message", message));
 						record.setFamily("family");
-						
+
 						List recordMutations = new ArrayList();
-						recordMutations.add(new RecordMutation(RecordMutationType.REPLACE_ENTIRE_RECORD, record));
-						RowMutation mutation = new RowMutation("nrt", keyStr , RowMutationType.REPLACE_ROW,	recordMutations);
+						recordMutations.add(new RecordMutation(
+								RecordMutationType.REPLACE_ENTIRE_RECORD,
+								record));
+						RowMutation mutation = new RowMutation("nrt", keyStr,
+								RowMutationType.REPLACE_ROW, recordMutations);
 						mutation.setRecordMutations(recordMutations);
 
 						return new Tuple2<String, RowMutation>(keyStr, mutation);
@@ -166,13 +186,16 @@ public class ConsumerEnQueue implements Serializable {
 
 					private static final long serialVersionUID = 88875777435L;
 					
+					/*
+					 * Blur Thrift Client
+					 */
+
 					Iface client = BlurClient.getClient("10.252.5.113:40010");
 
 					@Override
 					public Void call(JavaPairRDD<String, RowMutation> rdd,
 							Time time) throws Exception {
 
-						
 						for (Tuple2<String, RowMutation> tuple : rdd.collect()) {
 
 							if (tuple != null) {
@@ -180,6 +203,10 @@ public class ConsumerEnQueue implements Serializable {
 								try {
 
 									RowMutation rm = tuple._2;
+									
+									/*
+									 * Index using enqueue mutate call
+									 */
 									client.enqueueMutate(rm);
 
 								} catch (Exception ex) {
